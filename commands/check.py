93696f623d43f61fd5849348b4725d46014bd451
Kamil <kamil@arch.localdomain>
2019-07-08 23:17:06 +0200

    Use settings in modules

--- commands/check.py
+++ commands/check.py
@@ -1,23 +1,26 @@
 from commands.base import BaseCommand as baseCommand
 import discord
+from playercheckmethod import PlayerCheckMethod
 class CheckCommand(baseCommand):
-   def __init__(self, discordClient, mention, permissionChecker, playersCheck):
+   def __init__(self, discordClient, mention, permissionChecker, playersCheck, settingManager):
       self.playersCheck = playersCheck
+      self.settingManager = settingManager
       super(CheckCommand, self).__init__(discordClient, mention, permissionChecker)
 
    async def Execute(self, member, args): 
       if self.permissionChecker.IsAdmin(member) is False:
-         return
+         return 
       channelToSend = None
       if len(args) <= 0:
          channelToSend = discord.utils.find(lambda c: c.name == "klaudiusz-testing", self.discordClient.get_all_channels())
       else:
          channelToSend = self.discordClient.get_channel(self.mention.getInt(args[0]))
-      usersToRemove = await self.playersCheck.Check([{'id': '572897687502848034', 'channels': ['572896095374409730']}], "JOIN_DATE", 2)
+      usersWithoutAccept = await self.playersCheck.Check([{'id': '572897687502848034', 'channels': ['572896095374409730']}], PlayerCheckMethod.JOIN_DATE, 2)
       if channelToSend is not None:
-         msg = "Przyjezdni bez zaakceptowanej KP od dwóch dni:\n"
-         for userId in usersToRemove:
-            msg = msg + "\n- <@{0}>".format(userId)
+         settingObj = self.settingManager.LoadSettings()
+         msg = "Przyjezdni bez KP od dwóch dni:\n"
+         for user in usersWithoutAccept:
+            msg = msg + "\n- <@{0}>".format(user.id)
          getIdsFromChannelsInCategories = ['573189641797238795', '573189424897064964', '573189367137566750', '573189223608352779', '573189156717592586', '573100293240258629']
          channelsToCheck = []
          thisGuild = self.discordClient.guilds[0]
@@ -25,9 +28,12 @@ class CheckCommand(baseCommand):
             category = discord.utils.find(lambda c: str(c.id) == categoryId, thisGuild.categories)
             for channel in category.channels:
                channelsToCheck.append(str(channel.id))
-         usersToRemove = await self.playersCheck.Check([{'id': '575301044695728158', 'channels': channelsToCheck}], "MESSAGE_AGO", 7)
+         inactiveUsers = await self.playersCheck.Check([{'id': '575301044695728158', 'channels': channelsToCheck}], PlayerCheckMethod.MESSAGE_ADD, 7)
          msg = msg + "\n\nGracze bez aktywnej sesji od 7 dni:"
-         for userId in usersToRemove:
-            msg = msg + "\n- <@{0}>".format(userId)
+         for user in inactiveUsers:
+            msg = msg + "\n- <@{0}>".format(user.id)
          msg = msg + "\n\nStwórca nie jest zadowolony..."         
-         await channelToSend.send(msg)
+         await channelToSend.send(msg)         
+
+
+         
--- permission.py
+++ permission.py
@@ -1,10 +1,12 @@
 class PermissionChecker():
-   def __init__(self):
+   def __init__(self, settingManager):
+      self.settingManager = settingManager
       pass
       
    def IsAdmin(self, member):
       allow = False
-      requiredRole = ['572892451107504157', '573268722777587722']
+      settingsObject = self.settingManager.LoadSettings()
+      requiredRole = settingsObject['adminRolesIds'] 
       for role in member.roles:
          if str(role.id) in requiredRole:
             allow = True
