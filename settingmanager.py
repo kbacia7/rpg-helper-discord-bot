463572cdd3a09c4ae48e20ac585b98079b4407c3
Kamil <kamil@arch.localdomain>
2019-07-08 23:14:49 +0200

    Load settings from file

---
+++ settingmanager.py
@@ -0,0 +1,36 @@
+import os
+import yaml
+from path import Path
+class SettingManager():
+   def __init__(self):
+      self.settings = None
+      pass
+   
+   def UpdateSettings(self, object): 
+      with open(Path.SETTINGS.value, 'w', encoding="utf-8") as settingFile:
+         yaml.dump(object, settingFile, allow_unicode=True)
+
+   def CreateSettings(self):
+      defaultSettingDict = {
+         'discordToken': '', 
+         'adminRolesIds': [], 
+         "checkedPlayers": [],
+         "msgForInactiveUsers": "",
+         "msgForUsersWithoutCharacter": ""  ,
+         "checkedInactiveUsers": {},
+         "checkedUsersWithoutAccept": {}
+      }
+      self.UpdateSettings(defaultSettingDict)
+      
+   def LoadSettings(self):
+      if self.settings is not None:
+         return self.settings
+      else:
+         if os.path.exists(Path.SETTINGS.value):
+            with open(Path.SETTINGS.value, 'r') as settingFile:
+               self.settings = yaml.load(settingFile, Loader=yaml.FullLoader)
+               return self.settings
+         else:
+            self.CreateSettings()
+            self.LoadSettings()
+      
