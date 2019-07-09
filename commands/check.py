8d04ba8879cbc7f293b236b93d8726d167dd73a5
Kamil <kamil@arch.localdomain>
2019-07-09 21:31:29 +0200

    Check sended messages by user

--- commands/check.py
+++ commands/check.py
@@ -1,26 +1,35 @@
 from commands.base import BaseCommand as baseCommand
 import discord
+import datetime
 from playercheckmethod import PlayerCheckMethod
+from getlastmessagemode import GetLastMessageMode
 class CheckCommand(baseCommand):
-   def __init__(self, discordClient, mention, permissionChecker, playersCheck, settingManager):
+   def __init__(self, discordClient, mention, permissionChecker, playersCheck, settingManager, getLastMessage):
       self.playersCheck = playersCheck
       self.settingManager = settingManager
+      self.getLastMessage = getLastMessage
       super(CheckCommand, self).__init__(discordClient, mention, permissionChecker)
 
    async def Execute(self, member, args): 
       if self.permissionChecker.IsAdmin(member) is False:
          return 
-      channelToSend = None
-      if len(args) <= 0:
-         channelToSend = discord.utils.find(lambda c: c.name == "klaudiusz-testing", self.discordClient.get_all_channels())
-      else:
-         channelToSend = self.discordClient.get_channel(self.mention.getInt(args[0]))
+      channelToSend = discord.utils.find(lambda c: c.name == "klaudiusz-testing", self.discordClient.get_all_channels())
+      fullMode = False
+      if len(args) > 0:
+         if args[0] == "full":
+            fullMode = True
       usersWithoutAccept = await self.playersCheck.Check([{'id': '572897687502848034', 'channels': ['572896095374409730']}], PlayerCheckMethod.JOIN_DATE, 2)
       if channelToSend is not None:
          settingObj = self.settingManager.LoadSettings()
          msg = "Przyjezdni bez KP od dwóch dni:\n"
          for user in usersWithoutAccept:
-            msg = msg + "\n- <@{0}>".format(user.id)
+            lastMessage = None
+            if fullMode:
+               lastMessage = await self.getLastMessage.FindMessageByUser(user, ["572893870766030898", "572894344080785446", "572895747956277279"], GetLastMessageMode.CATEGORIES)
+            if lastMessage is not None:
+               msg = msg + "\n- <@{0}> (ostatnia wiadomość {1} dni temu na <#{2}>)".format(user.id, (datetime.datetime.now() - lastMessage.created_at).days, lastMessage.channel.id)
+            else:
+               msg = msg + "\n- <@{0}>".format(user.id)
          getIdsFromChannelsInCategories = ['573189641797238795', '573189424897064964', '573189367137566750', '573189223608352779', '573189156717592586', '573100293240258629']
          channelsToCheck = []
          thisGuild = self.discordClient.guilds[0]
@@ -31,7 +40,13 @@ class CheckCommand(baseCommand):
          inactiveUsers = await self.playersCheck.Check([{'id': '575301044695728158', 'channels': channelsToCheck}], PlayerCheckMethod.MESSAGE_ADD, 7)
          msg = msg + "\n\nGracze bez aktywnej sesji od 7 dni:"
          for user in inactiveUsers:
-            msg = msg + "\n- <@{0}>".format(user.id)
+            lastMessage = None
+            if fullMode:
+               lastMessage = await self.getLastMessage.FindMessageByUser(user, ["572893870766030898", "572894344080785446", "572895747956277279"], GetLastMessageMode.CATEGORIES)
+            if lastMessage is not None:
+               msg = msg + "\n- <@{0}> (ostatnia wiadomość {1} dni temu na <#{2}>)".format(user.id, (datetime.datetime.now() - lastMessage.created_at).days, lastMessage.channel.id)
+            else:
+               msg = msg + "\n- <@{0}>".format(user.id)
          msg = msg + "\n\nStwórca nie jest zadowolony..."         
          await channelToSend.send(msg)         
 
