5f9bb0297ee869743e4e30f47f53f0e5519f0062
Kamil <kamil@arch.localdomain>
2019-07-10 19:50:43 +0200

    Remove saved players id's

--- client.py
+++ client.py
@@ -1,9 +1,10 @@
 import discord
-
+import datetime
 class DiscordClientBase(discord.Client):
-    def __init__(self, commandExecutor, argparser):
+    def __init__(self, commandExecutor, argparser, settingManager):
         self.commandExecutor = commandExecutor
         self.argparser = argparser
+        self.settingManager = settingManager
         super().__init__()
 
 class ExDiscordClient(DiscordClientBase):
@@ -13,8 +14,35 @@ class ExDiscordClient(DiscordClientBase):
         print('Logged on as {0}!'.format(self.user))
 
     async def on_message(self, message):
+        settingObj = self.settingManager.LoadSettings()
         if message is None or len(message.content) <= 0 or message.author == self.user:
            return
+        if message.channel.category.id in settingObj['categoriesForLookingInactivePlayers'] and str(message.author.id) in settingObj['checkedInactiveUsers']:
+            del settingObj['checkedInactiveUsers'][str(message.author.id)]
+            settingObj.UpdateSettings(settingObj)
+
         if message.content[0] == '/':
            data = self.argparser.Parse(message.content)
            await self.commandExecutor.Execute(data[0], message.author, data[1:])
+
+    async def on_member_update(self, before, after):
+        beforeRolesIds = [str(r.id) for r in before.roles]
+        afterRolesIds = [str(r.id) for r in after.roles]
+        playerRole = '575301044695728158'
+        if playerRole not in beforeRolesIds and playerRole in afterRolesIds:
+           settingObj = self.settingManager.LoadSettings()
+           settingObj['playerFromDate'][str(after.id)] = datetime.datetime.now()
+           if str(after.id) in settingObj['playerFromDate']:
+              del settingObj['checkedUsersWithoutAccept'][str(after.id)]
+           self.settingManager.UpdateSettings(settingObj)
+
+    async def on_member_remove(self, member):
+        settingObj = self.settingManager.LoadSettings()
+        memberId = str(member.id)
+        if memberId in settingObj['checkedInactiveUsers']:
+           del settingObj['checkedInactiveUsers'][memberId] 
+        if memberId in settingObj['checkedUsersWithoutAccept']:
+           del settingObj['checkedUsersWithoutAccept'][memberId]
+        if memberId in settingObj['playerFromDate']:
+           del settingObj['playerFromDate'][memberId]
+        settingObj.UpdateSettings(settingObj)
