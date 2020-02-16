50ef11593574ac2b12e31da8efdc9ca8dd5dfec8
kerorero <kerorero13@gmail.com>
2020-02-16 16:22:18 +0100

    ServerSetting model with migration

---
+++ rpgdiscordhelper/models/ServerSetting.py
@@ -0,0 +1,8 @@
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Text
+from rpgdiscordhelper.models.Base import Base
+class ServerSetting(Base):
+    __tablename__ = 'servers_settings'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
+    value = Column(Text(), nullable=False)
+    server_id = Column(String(32), nullable=False)
