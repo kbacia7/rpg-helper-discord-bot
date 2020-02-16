29a960f9b71e768279f203a8156553227695322d
kerorero <kerorero13@gmail.com>
2020-02-16 16:23:02 +0100

    Basic settings for alembic

---
+++ alembic/versions/a9d958f29cf3_initialize.py
@@ -0,0 +1,24 @@
+"""Initialize
+
+Revision ID: a9d958f29cf3
+Revises: 
+Create Date: 2020-02-16 16:08:29.478021
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a9d958f29cf3'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
